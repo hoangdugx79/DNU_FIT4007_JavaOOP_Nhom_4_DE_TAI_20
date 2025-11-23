93222484eaf439c340922403a4c30a492fd2c199
Dinh Duc Tho <dinhductho2005kk@gmail.com>
2025-11-24 06:04:56 +0700

    ductho
    Define Persistable interface for CSV data operations

--- src/interfaces/Persistable.java
+++ src/interfaces/Persistable.java
@@ -1,4 +1,33 @@
 package interfaces;
 
-public class Persistable {
+import java.io.IOException;
+
+/**
+ * Interface cho các class cần lưu trữ và đọc dữ liệu từ file CSV
+ * Repository classes sẽ implement interface này
+ */
+public interface Persistable {
+
+    /**
+     * Lưu toàn bộ dữ liệu vào file CSV
+     * @throws IOException nếu có lỗi khi ghi file
+     */
+    void save() throws IOException;
+
+    /**
+     * Đọc toàn bộ dữ liệu từ file CSV
+     * @throws IOException nếu có lỗi khi đọc file
+     */
+    void load() throws IOException;
+
+    /**
+     * Lấy đường dẫn file CSV
+     * @return đường dẫn đầy đủ của file
+     */
+    String getFilePath();
+
+    /**
+     * Xóa toàn bộ dữ liệu (clear cache trong memory)
+     */
+    void clear();
 }
