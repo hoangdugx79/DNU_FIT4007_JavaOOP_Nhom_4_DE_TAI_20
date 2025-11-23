4d01d09324f893f580af28abafcae660982f6adb
Dinh Duc Tho <dinhductho2005kk@gmail.com>
2025-11-24 06:04:19 +0700

    ductho

--- src/exception/OrderNotFoundException.java
+++ src/exception/OrderNotFoundException.java
@@ -1,4 +1,24 @@
 package exception;
 
-public class OrderNotFoundException {
+/**
+ * Exception được ném ra khi không tìm thấy đơn hàng (nhập/xuất) theo ID
+ */
+public class OrderNotFoundException extends Exception {
+    private String orderId;
+    private String orderType; // "IMPORT" hoặc "EXPORT"
+
+    public OrderNotFoundException(String orderId, String orderType) {
+        super(String.format("Không tìm thấy đơn %s với ID: %s",
+                orderType.equals("IMPORT") ? "nhập kho" : "xuất kho", orderId));
+        this.orderId = orderId;
+        this.orderType = orderType;
+    }
+
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+
+    // Getters
+    public String getOrderId() { return orderId; }
+    public String getOrderType() { return orderType; }
 }
